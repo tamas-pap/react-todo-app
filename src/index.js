721c756b4f8b62a8fe8c870216c5cd54d946ebc3
Tamas Pap <tamas@tamaspap.com>
2018-11-21 16:54:35 +0200

    Install polished and styled-components.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const App = () => <h1>Our TodoList comes here..</h1>;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
