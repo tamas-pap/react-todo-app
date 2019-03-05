98761127312f63f3ed29cdb988da87a193b3f4d3
CataNedelcu <catalina.nedelcu@xoomworks.com>
2019-03-05 15:38:09 +0200

    WIP

--- src/todoLists/ducks/index.js
+++ src/todoLists/ducks/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import { reducer as todoListsReducer } from './todoLists';
 
-export { createTodoList } from './todoLists';
+export { createTodoList, loadTodoLists } from './todoLists';
 
 export const reducer = combineReducers({
   todoLists: todoListsReducer,
